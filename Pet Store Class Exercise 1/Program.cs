ebf149516aaa83375343d50d1980f8f1fa381781
Edward Curtis <gamerguy1835@yahoo.com>
2024-10-30 14:11:24 -0400

    Added better control flow to the program

--- Pet Store Class Exercise 1/Program.cs
+++ Pet Store Class Exercise 1/Program.cs
@@ -12,15 +12,25 @@ using Pet_Store_Class_Exercise_1;
         if( productLogic.GetDogLeashByName("") != null)
         {
             Console.WriteLine("Enter Dog Leash Name");
+
             productLogic.GetDogLeashByName("").Name = userInput;
-            Console.WriteLine("Enter Dog Leash description");
-            
+          
         }
      }
+
+    /*
+     *   Console.WriteLine("Enter Dog Leash description");
+            productLogic.GetDogLeashByName("").Name = userInput;
+            Console.WriteLine("Enter Dog Leash description");
+            productLogic.GetDogLeashByName()
+     * 
+     * 
+     */
     private static void EnterCatFood(ProductLogic productLogic, string? userInput)
     {
         if (productLogic.GetCatFoodByName("") != null)
         {
+            Console.WriteLine("Enter Cat Food Name");
             productLogic.GetCatFoodByName("").Name = userInput;
         }
     }
@@ -58,30 +68,123 @@ using Pet_Store_Class_Exercise_1;
 
 
 
-          
+
 
             try
             {
-              if(userInput == "1")
+                if (userInput == "1")
                 {
                     Console.WriteLine("Please enter the type of product press type 'dogleash' for leash ; type 'catfood for catfood");
+                    userInput = Console.ReadLine();
+                    if(userInput == "dogleash")
+                    {
+                        userInput = Console.ReadLine();
+                    }
+                    else if(userInput == "cat food")
+                    {
+                        userInput = Console.ReadLine();
+                        CatFood catFood = new CatFood();
+                        userInput = Console.ReadLine();
+                        catFood.Name = userInput;
+                        userInput = Console.ReadLine();
+
+                    }
                 }
-                if(userInput.Trim().ToLower() == "dogleash")
+
+                else if (userInput == "8")
                 {
-                    DogLeash dogLeash = new DogLeash();
-                    productLogic.AddProduct(dogLeash);
-                    Console.WriteLine("dogLeash Selected enter dogleash parameters");
+                    Console.WriteLine("Type 2 to find dog leash by name");
+                    Console.WriteLine("Tyoe 3 to find catfood by name");
+                    userInput = Console.ReadLine();
+                    if (userInput?.ToLower().Trim() == "2")
+                    {
+                        DogLeash dogLeash = null;
+                        Console.WriteLine("Enter Name of Dog Leash");
+                        userInput = Console.ReadLine();
+                        try
+                        {
+                            dogLeash = productLogic.GetDogLeashByName(userInput);
+
+                        }
+                        catch(Exception ex)
+                        {
+                            continue;
+                        }
+                        if (dogLeash != null)
+                        {
+                            Console.WriteLine($"{dogLeash.ToString()}");
+
+                        }
+
+                    }
+                   else if (userInput?.ToLower().Trim() == "3")
+                    {
+                        CatFood catFood = null;
+                        Console.WriteLine("Enter Name of cat food");
+                        userInput = Console.ReadLine();
+                        try
+                        {
+                            catFood = productLogic.GetCatFoodByName(userInput);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            continue;
+                        }
+                        if (catFood != null)
+                        {
+                            Console.WriteLine($"{catFood.ToString()}");
+
+                        }
+
+                    }
+                    /*
+                   else if (userInput.ToLower().Trim() == "3")
+                   {
+                       userInput = Console.ReadLine();
+                       if (productLogic.GetCatFoodByName(userInput) != null)
+                       {
+
+
+                           Console.WriteLine($"Catfood {productLogic.GetCatFoodByName(userInput).ToString()}");
+                       }
+                       else
+                       {
+                           Console.WriteLine("Cat Food Not Fount");
+                           continue;
+                       }
+
+               }
+
+
+
+               else if(userInput.Trim().ToLower() == "dogleash")
+               {
+                   DogLeash dogLeash = new DogLeash();
+                   productLogic.AddProduct(dogLeash);
+                   Console.WriteLine("dogLeash Selected enter dogleash parameters");
+               }
+               else if (userInput.Trim().ToLower() == "catfood")
+               {
+                   CatFood catFood = new CatFood();
+                   productLogic.AddProduct(catFood);
+                   Console.WriteLine("Catfood selected enter catfood parameters");
+               }
+              else
+               {
+                   Console.WriteLine("Enter dogl to add paremeters");
+               }
+               if (userInput.ToLower().Trim() == "dogl")
+               {
+
+
+                   EnterDogFood(productLogic, userInput);
+               }
+                */
+                    userInput = Console.ReadLine();
                 }
-                else if (userInput.Trim().ToLower() == "catfood")
-                {
-                    CatFood catFood = new CatFood();
-                    productLogic.AddProduct(catFood);
-                    Console.WriteLine("Catfood selected enter catfood parameters");
-                }
-                Console.WriteLine("Enter dogl to add paremeters");
-                
-                userInput = Console.ReadLine();
             }
+
             catch (FormatException ex)
             {
                 Console.WriteLine(ex.Message);
--- Pet Store Class Exercise 1/obj/Debug/net8.0/Pet Store Class Exercise 1.AssemblyInfo.cs
+++ Pet Store Class Exercise 1/obj/Debug/net8.0/Pet Store Class Exercise 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pet Store Class Exercise 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af0b9e841e179135aa05a10d37f60a965f09696")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc0b3322c668a283221bd5cb8acd4e8279ea2f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pet Store Class Exercise 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pet Store Class Exercise 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
